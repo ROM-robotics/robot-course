7c5369e3c7b8671400ed6c32315425e1ce9985ea
GreenGhostMan <server01.psa1981@gmail.com>
2019-06-04 07:39:58 +0630

    chapter02 add

---
+++ chapter02/psa_server/src/demo_client.cpp
@@ -0,0 +1,38 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <actionlib/client/simple_action_client.h>
+#include <psa_server/demoAction.h>
+#include <actionlib/client/terminal_state.h>
+
+int main(int argc,char** argv)
+{
+	ros::init(argc,argv,"demo_client");
+	if(argc != 3)
+	{
+		ROS_WARN("Usage: demo_client <goal> <time_to_preempt>");
+		return -1;
+	}
+	actionlib::SimpleActionClient<psa_server::demoAction> ac("demo_action",true);
+	ROS_INFO("Waiting for action server ...");
+
+	ac.waitForServer();
+	psa_server::demoGoal goal;
+	goal.count=atoi(argv[1]);
+
+	ROS_INFO("Sending goal %d and preempt time of %d",goal.count,atoi(argv[2]) );
+	ac.sendGoal(goal);
+
+	bool status = ac.waitForResult(ros::Duration(atoi(argv[2])));
+	ac.cancelGoal();
+
+	if(status)
+	{
+		actionlib::SimpleClientGoalState state = ac.getState();
+		ROS_INFO("Action finished %s",state.toString().c_str());
+		ac.cancelGoal();
+	}
+	else{
+		ROS_INFO("Action did not finish before the time out");
+	}
+
+}
---
+++ chapter02/psa_server/src/demo_server.cpp
@@ -0,0 +1,79 @@
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+#include <actionlib/server/simple_action_server.h>
+#include "psa_server/demoAction.h"
+#include <iostream>
+#include <sstream>
+
+class DemoAction{
+protected:
+	ros::NodeHandle nh;
+	actionlib::SimpleActionServer<psa_server::demoAction> as;
+
+	psa_server::demoFeedback feedback;
+	psa_server::demoResult result;
+
+	std::string action_name;
+	int goal;
+	int progress;
+public:
+	DemoAction(std::string name) : as(
+		nh,
+		name,
+		boost::bind(&DemoAction::executeCB,this,_1),
+		false),
+	action_name(name)
+	{
+		as.registerPreemptCallback(boost::bind(&DemoAction::preemptCB,this));
+		as.start();
+	}
+
+	void preemptCB()
+	{
+		ROS_WARN("%s got preempted!", action_name.c_str());
+		result.final_count=progress;
+		as.setPreempted(result,"I got Preempted");
+	}
+	void executeCB(const psa_server::demoGoalConstPtr &goal)
+	{
+		if(!as.isActive() || as.isPreemptRequested()) return;
+
+		ros::Rate r(5);
+		ROS_INFO("%s is processing the goal %d",action_name.c_str(),goal->count);
+
+		for(progress =1;progress<=goal->count;progress++)
+		{
+			if(!ros::ok())
+			{
+				result.final_count = progress;
+				as.setAborted(result,"I fail");
+				ROS_INFO("%s shutting dodwn",action_name.c_str());
+				break;
+			}
+			if(!as.isActive() || as.isPreemptRequested() ) return;
+
+			if(goal->count<=progress)
+			{
+				result.final_count = progress;
+				as.setSucceeded(result);
+
+			}
+			else{
+				feedback.current_count = progress;
+				as.publishFeedback(feedback);
+			}
+			r.sleep();
+		}
+	}
+
+};
+
+
+int main(int argc,char** argv)
+{
+	ros::init(argc,argv,"demo_action");
+	ROS_INFO("Starting Action server");
+	DemoAction da(ros::this_node::getName());
+	ros::spin();
+	return 0;
+}
